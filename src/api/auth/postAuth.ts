39fdc5dab995d46a1bd189a8daa3597d36347f02
Inggis Kurnia Trisiawan <inggiskurnia32@gmail.com>
2025-05-04 22:58:03 +0700

    feat: logout

--- src/api/auth/postAuth.ts
+++ src/api/auth/postAuth.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { LoginForm, LoginResponse } from "@/types/auth";
 import { ApiResponse } from "@/types/response";
+import { getSession } from "next-auth/react";
 
 const authUrl = `${process.env.NEXT_PUBLIC_BACKEND_URL}${process.env.NEXT_PUBLIC_AUTH}`;
 
@@ -10,6 +11,28 @@ export const loginUser = async (
   const response = await axios.post<ApiResponse<LoginResponse>>(
     `${authUrl}/login`,
     body,
+  );
+
+  return response.data;
+};
+
+export const logoutUser = async (): Promise<ApiResponse<LoginResponse>> => {
+  const session = await getSession();
+  const accessToken = session?.accessToken;
+  const refreshToken = session?.refreshToken;
+
+  if (!accessToken) throw new Error("No access token");
+
+  const response = await axios.post<ApiResponse<LoginResponse>>(
+    `${authUrl}/logout`,
+    {
+      refreshToken: refreshToken,
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    },
   );
 
   return response.data;
--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,9 +1,34 @@
+"use client";
+
 import { FC } from "react";
-import { LogOut } from "lucide-react";
+import { LogIn, LogOut } from "lucide-react";
 import AvatarComponent from "@/components/common/AvatarComponent";
 import Link from "next/link";
+import { signOut, useSession } from "next-auth/react";
+import { logoutUser } from "@/api/auth/postAuth";
+import { toast } from "@/hooks/use-toast";
 
 const Navbar: FC = () => {
+  const session = useSession();
+
+  const handleLogout = () => {
+    logoutUser()
+      .then(() => {
+        toast({
+          title: "Logout successful",
+          duration: 5000,
+        });
+        signOut({ callbackUrl: "/login" });
+      })
+      .catch(() => {
+        toast({
+          title: "Logout failed",
+          variant: "destructive",
+          duration: 5000,
+        });
+      });
+  };
+
   return (
     <nav
       className={
@@ -21,13 +46,26 @@ const Navbar: FC = () => {
       </Link>
 
       <div>
-        <button
-          className={
-            "cursor-pointer font-semibold rounded-md flex items-center gap-2 text-gray-700"
-          }
-        >
-          Logout <LogOut size={"18"} />
-        </button>
+        {!session.data ? (
+          <Link href={"/login"}>
+            <button
+              className={
+                "cursor-pointer font-semibold rounded-md flex items-center gap-2 text-gray-700"
+              }
+            >
+              <LogIn size={18} /> Login
+            </button>
+          </Link>
+        ) : (
+          <button
+            className={
+              "cursor-pointer font-semibold rounded-md flex items-center gap-2 text-gray-700"
+            }
+            onClick={handleLogout}
+          >
+            Logout <LogOut size={"18"} />
+          </button>
+        )}
       </div>
     </nav>
   );
